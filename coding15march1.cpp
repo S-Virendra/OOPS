d5d45dcfcf5af9ab25a148c616eda97e3cb82eac
VIRENDRA SINGH PAL <palvirendrasingh697@gmail.com>
2025-04-04 23:51:23 +0530

    sum

---
+++ coding15march1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1;
+    int n2;
+    int sum;
+   
+    
+    int digit=n2-n1+1;
+    cin>>n1>>n2;
+    int value=digit/2;
+    int firlast=n1+n2;
+    sum=value*firlast;
+    cout<<sum;
+}
