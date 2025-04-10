f03f4beb0a80348a8aece6fdcb84fc92207863ce
VIRENDRA SINGH PAL <palvirendrasingh697@gmail.com>
2025-04-10 23:31:42 +0530

    frequencyofelement

---
+++ frequency_of_elementinarray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void countFrequency(int *arr,int n){
+    vector<bool>visited(n,false);
+    for(int i=0;i<n;i++){
+        if(visited[i]==true)
+        continue;
+        int count=1;
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){
+                visited[j]=true;
+                count++;
+            }
+        }
+        cout<<arr[i]<<" "<<count<<endl;
+    }
+
+}
+int main(){
+    int arr[]={10,5,10,15,10,5};
+    int n=sizeof(arr)/sizeof(int);
+    countFrequency(arr,n);
+}
